3ad9281e19de0c8f501f770a28aafc88827cebcc
jeffren716 <jeffyiren@gmail.com>
2017-11-24 14:08:54 -0800

    position stream on page

--- frontend/components/channel/channel_index.jsx
+++ frontend/components/channel/channel_index.jsx
@@ -6,16 +6,17 @@ class ChannelIndex extends React.Component {
 
   render() {
     return(
-      <iframe
-        src='http://player.twitch.tv/?video=187283482&time=03m46s'
-        height='720'
-        width='1280'
-        frameBorder='0'
-        scrolling='no'
-        allowFullScreen='true'
-        time='03m46s'>
-
-      </iframe>
+      <div className='user-stream'>
+        <iframe
+          src='http://player.twitch.tv/?video=187283482&time=03m46s'
+          height='720'
+          width='1280'
+          frameBorder='0'
+          scrolling='no'
+          allowFullScreen='true'
+          time='03m46s'>
+        </iframe>
+      </div>
     );
   }
 }
