f916662963c1abbc59ec4e44b67da34c96fdb586
jeffren716 <jeffyiren@gmail.com>
2017-11-28 13:43:21 -0800

    prepare push to heroku

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,10 +1,10 @@
 class Channel < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many :users,
-  primary_key: :id,
-  foreign_key: :channel_id,
-  class_name: :User,
-  validate: false
+  # has_many :users,
+  # primary_key: :id,
+  # foreign_key: :channel_id,
+  # class_name: :User,
+  # validate: false
 
 end
