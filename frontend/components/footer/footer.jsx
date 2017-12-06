701ef09f66e8ca8d3443f79947233242b244780f
jeffren716 <jeffyiren@gmail.com>
2017-12-05 17:53:49 -0800

    edit linkedin url

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <img className='github-image' src='https://image.flaticon.com/icons/png/512/25/25231.png'></img>
       </a>
 
-      <a href='https://www.linkedin.com/in/jeffrey-ren-a71b48108/' target="_blank">
+      <a href='https://www.linkedin.com/in/jeffrey-ren/' target="_blank">
         <img className='linkedin-image' src='http://www.freepngimg.com/download/linkedin/9-2-linkedin-free-download-png.png'></img>
       </a>
     </div>
